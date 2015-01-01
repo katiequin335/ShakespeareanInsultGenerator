4b982db50679718cc51d6ed3c8bbdcabab17f756
katiequin335 <katiequin335@gmail.com>
2015-01-01 17:21:57 -0500

    Create Shakespeare.java

---
+++ Shakespeare.java
@@ -0,0 +1,326 @@
+import java.util.Random;
+
+public class Shakespeare{
+
+	public static void main(String[] args){
+
+		Random r = new Random();
+
+		//column 1
+		int choice1 = 1 + r.nextInt(50);
+		String response1 = "";
+
+		if (choice1==1)
+			response1="artless ";
+		if (choice1==2)
+			response1="bawdy ";
+		if (choice1==3)
+			response1="beslubbering ";
+		if (choice1==4)
+			response1="bootless ";
+		if (choice1==5)
+			response1="churlish ";
+		if (choice1==6)
+			response1="cockered ";
+		if (choice1==7)
+			response1="clouted ";
+		if (choice1==8)
+			response1="craven ";
+		if (choice1==9)
+			response1="currish ";
+		if (choice1==10)
+			response1="dankish ";
+		if (choice1==11)
+			response1="dissembling ";
+		if (choice1==12)
+			response1="droning ";
+		if (choice1==13)
+			response1="errant ";
+		if (choice1==14)
+			response1="fawning ";
+		if (choice1==15)
+			response1="fobbing ";
+		if (choice1==16)
+			response1="froward ";
+		if (choice1==17)
+			response1="frothy ";
+		if (choice1==18)
+			response1="gleeking ";
+		if (choice1==19)
+			response1="goatish ";
+		if (choice1==20)
+			response1="gorbellied ";
+		if (choice1==21)
+			response1="impertinent ";
+		if (choice1==22)
+			response1="infectious ";
+		if (choice1==23)
+			response1="jarring ";
+		if (choice1==24)
+			response1="loggerheaded ";
+		if (choice1==25)
+			response1="lumpish ";
+		if (choice1==26)
+			response1="mammering ";
+		if (choice1==27)
+			response1="mangled ";
+		if (choice1==28)
+			response1="mewling ";
+		if (choice1==29)
+			response1="paunchy";
+		if (choice1==30)
+			response1="pribbling ";
+		if (choice1==31)
+			response1="puking ";
+		if (choice1==32)
+			response1="puny ";
+		if (choice1==33)
+			response1="qualling ";
+		if (choice1==34)
+			response1="rank ";
+		if (choice1==35)
+			response1="reeky ";
+		if (choice1==36)
+			response1="roguish ";
+		if (choice1==37)
+			response1="ruttish ";
+		if (choice1==38)
+			response1="saucy ";
+		if (choice1==39)
+			response1="spleeny ";
+		if (choice1==40)
+			response1="spongy ";
+		if (choice1==41)
+			response1="surly ";
+		if (choice1==42)
+			response1="tottering ";
+		if (choice1==43)
+			response1="unmuzzled ";
+		if (choice1==44)
+			response1="vain ";
+		if (choice1==45)
+			response1="venomed ";
+		if (choice1==46)
+			response1="villainous ";
+		if (choice1==47)
+			response1="warped ";
+		if (choice1==48)
+			response1="wayward ";
+		if (choice1==49)
+			response1="weedy ";
+		if (choice1==50)
+			response1="yeasty ";
+
+		//column2
+		int choice2 = 1 + r.nextInt(50);
+		String response2 = "";
+
+		if (choice2==1)
+			response2="base-court ";
+		if (choice2==2)
+			response2="bat-fowling ";
+		if (choice2==3)
+			response2="beef-witted ";
+		if (choice2==4)
+			response2="beetle-headed ";
+		if (choice2==5)
+			response2="clapper-clawed ";
+		if (choice2==6)
+			response2="clay-brained ";
+		if (choice2==7)
+			response2="common-kissing ";
+		if (choice2==8)
+			response2="crook-pated ";
+		if (choice2==9)
+			response2="dismal-dreaming ";
+		if (choice2==10)
+			response2="dizzy-eyed ";
+		if (choice2==11)
+			response2="doghearted ";
+		if (choice2==12)
+			response2="dread-bolted ";
+		if (choice2==13)
+			response2="earth-vexing ";
+		if (choice2==14)
+			response2="elf-skinned ";
+		if (choice2==15)
+			response2="fat-kidneyed ";
+		if (choice2==16)
+			response2="fen-sucked ";
+		if (choice2==17)
+			response2="flap-mouthed ";
+		if (choice2==18)
+			response2="fly-bitten ";
+		if (choice2==19)
+			response2="folly-fallen ";
+		if (choice2==20)
+			response2="fool-born ";
+		if (choice2==21)
+			response2="full-gorged ";
+		if (choice2==22)
+			response2="guts-griping ";
+		if (choice2==23)
+			response2="half-faced ";
+		if (choice2==24)
+			response2="hasty-witted ";
+		if (choice2==25)
+			response2="hedge-born ";
+		if (choice2==26)
+			response2="hell-hated ";
+		if (choice2==27)
+			response2="idle-headed ";
+		if (choice2==28)
+			response2="ill-breeding ";
+		if (choice2==29)
+			response2="ill-nutured ";
+		if (choice2==30)
+			response2="knotty-pated ";
+		if (choice2==31)
+			response2="milk-livered ";
+		if (choice2==32)
+			response2="motley-minded ";
+		if (choice2==33)
+			response2="onion-eyed ";
+		if (choice2==34)
+			response2="plume-plucked ";
+		if (choice2==35)
+			response2="pottle-deep ";
+		if (choice2==36)
+			response2="pox-marked ";
+		if (choice2==37)
+			response2="reeling-ripe ";
+		if (choice2==38)
+			response2="rough-hewn ";
+		if (choice2==39)
+			response2="rude-growing ";
+		if (choice2==40)
+			response2="rump-fed ";
+		if (choice2==41)
+			response2="shard-borne ";
+		if (choice2==42)
+			response2="sheep-biting ";
+		if (choice2==43)
+			response2="spur-galled ";
+		if (choice2==44)
+			response2="swag-bellied ";
+		if (choice2==45)
+			response2="tardy-gaited ";
+		if (choice2==46)
+			response2="tickle-brained ";
+		if (choice2==47)
+			response2="toad-spotted ";
+		if (choice2==48)
+			response2="unchin-snouted ";
+		if (choice2==49)
+			response2="weather-bitten ";
+		if (choice2==50)
+			response2="boil-brained ";
+
+		//column 3
+		int choice3 = 1 + r.nextInt(50);
+		String response3 = "";
+
+		if (choice3==1)
+			response3="apple-john";
+		if (choice3==2)
+			response3="baggage";
+		if (choice3==3)
+			response3="barnacle";
+		if (choice3==4)
+			response3="bladder";
+		if (choice3==5)
+			response3="boar-pig";
+		if (choice3==6)
+			response3="bugbear";
+		if (choice3==7)
+			response3="bum-bailey";
+		if (choice3==8)
+			response3="canker-blossom";
+		if (choice3==9)
+			response3="clack-dish";
+		if (choice3==10)
+			response3="clotpole";
+		if (choice3==11)
+			response3="coxcomb";
+		if (choice3==12)
+			response3="codpiece";
+		if (choice3==13)
+			response3="death-token";
+		if (choice3==14)
+			response3="dewberry";
+		if (choice3==15)
+			response3="flap-dragon";
+		if (choice3==16)
+			response3="flax-wench";
+		if (choice3==17)
+			response3="flirt-gill";
+		if (choice3==18)
+			response3="foot-licker";
+		if (choice3==19)
+			response3="fustilarian";
+		if (choice3==20)
+			response3="giglet";
+		if (choice3==21)
+			response3="gudgeon";
+		if (choice3==22)
+			response3="haggard";
+		if (choice3==23)
+			response3="harpy";
+		if (choice3==24)
+			response3="hedge-pig";
+		if (choice3==25)
+			response3="horn-beast";
+		if (choice3==26)
+			response3="hugger-mugger";
+		if (choice3==27)
+			response3="joithead";
+		if (choice3==28)
+			response3="lewdster";
+		if (choice3==29)
+			response3="lout";
+		if (choice3==30)
+			response3="maggot-pie";
+		if (choice3==31)
+			response3="malt-worm";
+		if (choice3==32)
+			response3="mammet";
+		if (choice3==33)
+			response3="measle";
+		if (choice3==34)
+			response3="minnow";
+		if (choice3==35)
+			response3="miscreant";
+		if (choice3==36)
+			response3="mold-warp";
+		if (choice3==37)
+			response3="mumble-news";
+		if (choice3==38)
+			response3="nut-hook";
+		if (choice3==39)
+			response3="piegon egg";
+		if (choice3==40)
+			response3="pignut";
+		if (choice3==41)
+			response3="puttock";
+		if (choice3==42)
+			response3="pumpion";
+		if (choice3==43)
+			response3="ratsbane";
+		if (choice3==44)
+			response3="scut";
+		if (choice3==45)
+			response3="skainsmate";
+		if (choice3==46)
+			response3="strumpet";
+		if (choice3==47)
+			response3="varlot";
+		if (choice3==48)
+			response3="vassal";
+		if (choice3==49)
+			response3="whey-face";
+		if (choice3==50)
+			response3="wagtail";
+
+		System.out.println("Thou " + response1 + response2 + response3 + "!");
+	}
+}
